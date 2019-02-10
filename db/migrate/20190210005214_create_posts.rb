513c82e043d0bc5242a3ae98230635a6a6023ad1
Matthew Martin <zhintateo19924559@gmail.com>
2019-02-09 17:14:16 -0800

    Configured the routes to Posts Index page

--- db/migrate/20190210005214_create_posts.rb
+++ db/migrate/20190210005214_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :task
+      t.string :task null: false
       t.date :date_started
       t.date :due_date
 
