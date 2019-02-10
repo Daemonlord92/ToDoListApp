50789ed6a9bbd9206e83475deb1d9d8c09587dc8
Matthew Martin <zhintateo19924559@gmail.com>
2019-02-09 17:32:14 -0800

    impletemented a test for Validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  	@post = Post.new(task: '
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.', date_started: Date.today, due_date: Date.today + 7)
+  end
+
+  subject { @post }
+
+  describe "when task is not present" do
+  	before { @post.task = " " }
+  	it { should_not be_valid }
+  end
 end
